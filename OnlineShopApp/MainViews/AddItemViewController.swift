7dd400b72b522fec8791989ecfba932aaf61c695
MCT <celaltok2@gmail.com>
2020-04-21 01:30:26 +0300

    imported Gallery,JGProgressHUD,NVAactibityIV and created extension of GalleryControllerDelegate and created showImageGallery func

--- OnlineShopApp/MainViews/AddItemViewController.swift
+++ OnlineShopApp/MainViews/AddItemViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Gallery
+import JGProgressHUD
+import NVActivityIndicatorView
 
 class AddItemViewController: UIViewController {
 
@@ -18,21 +21,34 @@ class AddItemViewController: UIViewController {
     
     //MARK: vars
     var category: Category!
+    var gallery: GalleryController!
+    let hud = JGProgressHUD(style: .dark)
+    
+    var activityIndicator: NVActivityIndicatorView?
+    
+    var itemImages : [UIImage?] = []
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print(category.name)
 
         // Do any additional setup after loading the view.
     }
     
     @IBAction func cameraClicked(_ sender: Any) {
+        itemImages.removeAll(keepingCapacity: false)
+        showImageGallery()
     }
     
     @IBAction func saveClicked(_ sender: Any) {
         dissmissKeyboard()
         
         if checkFieldAreCompleted() {
-                    
+            savetoFirebase()
         }else{
             makeAlert(title: "Error", message: "Title/Price/Description?")
         }
@@ -43,6 +59,8 @@ class AddItemViewController: UIViewController {
         dissmissKeyboard()
     }
     
+    
+   
     
     //MARK: Helper func
     
@@ -60,4 +78,75 @@ class AddItemViewController: UIViewController {
         alert.addAction(okButton)
         present(alert, animated: true, completion: nil)
     }
+    
+     //MARK: back to itemstableVC
+    private func popTheView(){
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    //MARK: Save Item
+    private func savetoFirebase() {
+        
+        let item = Item()
+        item.id = UUID().uuidString
+        item.name = titleTF.text!
+        item.price = Double(priceTF.text!)
+        item.categoryId = category.id
+        item.description = descriptionTextView.text
+        
+        if itemImages.count > 0 && !itemImages.isEmpty {
+            
+        }else {
+            saveItemToFirestore(item)
+            popTheView()
+        }
+    }
+    
+    //MARK: show Galery
+    private func showImageGallery(){
+        
+        self.gallery = GalleryController()
+        self.gallery.delegate = self
+        
+        Config.tabsToShow = [.imageTab,.cameraTab]
+        Config.Camera.imageLimit = 4
+        
+        self.present(self.gallery, animated: true, completion: nil)
+        
+    }
+    
+    
+}
+
+extension AddItemViewController: GalleryControllerDelegate {
+    
+    func galleryController(_ controller: GalleryController, didSelectImages images: [Image]) {
+        
+        if images.count > 0 {
+            
+            Image.resolve(images: images) { (resolvedImages) in
+                self.itemImages = resolvedImages
+            }
+        }
+        
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func galleryController(_ controller: GalleryController, didSelectVideo video: Video) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func galleryController(_ controller: GalleryController, requestLightbox images: [Image]) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    func galleryControllerDidCancel(_ controller: GalleryController) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
 }
