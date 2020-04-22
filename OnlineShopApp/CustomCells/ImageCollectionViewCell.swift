0adeb9f737417123dd2bdfc7bb547678cc453b26
MCT <celaltok2@gmail.com>
2020-04-23 00:11:46 +0300

    added downloadPic func and extention for imageSize

--- OnlineShopApp/CustomCells/ImageCollectionViewCell.swift
+++ OnlineShopApp/CustomCells/ImageCollectionViewCell.swift
@@ -10,4 +10,10 @@ import UIKit
 
 class ImageCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    func setupImageWith(itemImage: UIImage){
+        imageView.image = itemImage 
+    }
 }
--- OnlineShopApp/MainViews/ItemViewController.swift
+++ OnlineShopApp/MainViews/ItemViewController.swift
@@ -25,11 +25,33 @@ class ItemViewController: UIViewController {
     var itemImages:[UIImage] = []
     let hud = JGProgressHUD(style: .dark)
      
+    private let sectionInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+    private let cellHeight : CGFloat = 196.0
+    private let itemsPerRow: CGFloat = 1
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupUI()
+        
+    }
+    
+    //MARK: Download Pictures
+    
+    private func downloadPictures() {
+        
+        if item != nil && item.imageLinks != nil {
+            downloadImages(imageURL: item.imageLinks) { (allImages) in
+                
+                if allImages.count > 0 {
+                    
+                    self.itemImages = allImages as! [UIImage]
+                    self.imageCollectionView.reloadData()
+                    
+                }
+            }
+        }
         
     }
     
@@ -44,9 +66,7 @@ class ItemViewController: UIViewController {
             nameLabel.text = item.name
             descriptionLabel.text = item.description
             priceLabel.text = convertToCurrency(item.price)
-            
-            
-           
+            downloadPictures()
         }
     
     }
@@ -57,15 +77,39 @@ extension ItemViewController: UICollectionViewDataSource, UICollectionViewDelega
     
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 0
+        return itemImages.count == 0 ? 1 : itemImages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        return UICollectionViewCell()
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ImageCollectionViewCell
+           
+        if itemImages.count > 0 {
+            cell.setupImageWith(itemImage: itemImages[indexPath.row])
+        }
+        
+        return cell
+    }
+
+}
+
+extension ItemViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let availableWidth = collectionView.frame.width - sectionInsets.left
+
+        return CGSize(width: availableWidth, height: cellHeight)
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        return sectionInsets
+    }
     
-    
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return sectionInsets.left
+    }
 }
+
